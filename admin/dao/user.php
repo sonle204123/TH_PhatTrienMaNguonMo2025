060b35391529b56acc78ec1f4de40758bfccdf7a
Lê Ngọc Sơn <lengocson.2004tq@gmail.com>
2025-12-22 18:05:24 +0700

    Delete admin directory

--- admin/dao/user.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once "pdo.php";
-
-function checkuser($username, $password) {
-    $sql = "SELECT * FROM nguoidung WHERE ten_nguoi_dung = ? AND mat_khau = ?";
-    return pdo_query_one($sql, $username, $password);
-   
-}
