32d2bfab348257e82793dc598aee448c48f96f72
Vinh Tran <tranvinh4464@gmail.com>
2025-12-22 17:41:39 +0700

    [1.0] feature : Hoàn thành Xây dựng giao diện Dashboard admin #2

---
+++ admin/config/connect.php
@@ -0,0 +1,54 @@
+<?php
+function connectdb(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+   // $database = "laptop";
+
+  
+    try
+    {
+        $conn = new PDO("mysql:host=$servername;dbname=latop", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo "Connected Successfully";
+        
+    }
+    catch(PDOException $e){
+        // echo "Connection failed: " . $e->getMessage();
+    }
+    return $conn;
+}
+
+
+// function selectSQl($sql){
+//     $conn=connectdb();
+//     $stmt=$conn->prepare($sql);
+//     $stmt->execute();
+//     $result=$stmt->fetchAll(PDO::FETCH_ASSOC);
+//     $conn=null;
+//     return $result;
+// }
+
+function selectSQL($sql, $params = []) {
+    $conn = connectdb();
+    $stmt = $conn->prepare($sql);
+    $stmt->execute($params);
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $conn = null;
+    return $result;
+}
+
+
+function execSQL($sql, $params = []) {
+    $conn = connectdb(); // Lấy kết nối cơ sở dữ liệu
+    $stmt = $conn->prepare($sql);
+    $result = $stmt->execute($params);
+    $conn = null; // Đóng kết nối sau khi thực thi
+    return $result;
+}
+
+
+
+
+
+?>
